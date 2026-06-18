216c5f5143eaf5f22475f3b7c13f99d819ef1e94
ChandanGuptaDevs <chandan.g@aadhcode.com>
2026-06-18 17:20:07 +0530

    Add Google Tag Manager (GTM-5N9MNM4C)

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,7 +67,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       lang="en"
       className={`${yatra.variable} ${cormorant.variable} ${manrope.variable}`}
     >
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5N9MNM4C');` }} />
+      </head>
       <body>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5N9MNM4C" height="0" width="0" style={{ display:"none", visibility:"hidden" }} />
+        </noscript>
         <Navbar />
         <main>{children}</main>
         <Footer />
